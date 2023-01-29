0d0ca75ebb754ed821628de8bb6ce58bdb0cfe67
makkahwi <suhaibahmadai@hotmail.com>
2023-01-29 08:14:38 +0300

    Turn Landing Page To ES6

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import styles from "@/styles/Home.module.css";
 import { Inter } from "@next/font/google";
 import Head from "next/head";
-import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -20,4 +17,6 @@ export default function Home() {
       </main>
     </>
   );
-}
+};
+
+export default Home;
